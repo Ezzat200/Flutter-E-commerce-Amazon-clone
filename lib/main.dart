24c0f1b28caa0dc8bacfdbd69d492702c6409391
Ezzat Effat <156718898+Ezzat200@users.noreply.github.com>
2024-03-16 15:47:24 +0200

    craete app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AmazonApp());
+}
+
+class AmazonApp extends StatelessWidget {
+  const AmazonApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+     
+      home:  Container(),
+    );
+  }
+}
